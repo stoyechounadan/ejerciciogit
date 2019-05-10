a6662a5757f91b3d977b83c4bcfc2b394edcc86c
stoyechounadan <stoyechounadan@hotmail.com>
2019-05-10 13:12:01 +0200

    ejercicio terminado todos los operadores

--- ejerciciogit.js
+++ ejerciciogit.js
@@ -17,11 +17,41 @@ function eventoClickOperacion(){
 
 
 function calculaExpresion(exp){
-  var operador=exp.indexOf("+");
-  var num1=exp.substring(0,operador);
-  var num2=exp.slice(operador+1);
+  var operadorMas=exp.indexOf("+");
+  var operadorMenos=exp.indexOf("-");
+  var operadorPor=exp.indexOf("*");
+  var operadorEntre=exp.indexOf("/");
+
+if(operadorMas!=-1){
+  var num1=exp.substring(0,operadorMas);
+  var num2=exp.slice(operadorMas+1);
   num1=parseInt(num1);
   num2=parseInt(num2);
   var r= num1+num2;
   return r;
+}else if(operadorMenos!=-1){
+  var num1=exp.substring(0,operadorMenos);
+  var num2=exp.slice(operadorMenos+1);
+  num1=parseInt(num1);
+  num2=parseInt(num2);
+  var r= num1-num2;
+  return r;
+}else if(operadorPor!=-1){
+  var num1=exp.substring(0,operadorPor);
+  var num2=exp.slice(operadorPor+1);
+  num1=parseInt(num1);
+  num2=parseInt(num2);
+  var r= num1*num2;
+  return r;
+}else if(operadorEntre!=-1){
+  var num1=exp.substring(0,operadorEntre);
+  var num2=exp.slice(operadorEntre+1);
+  num1=parseInt(num1);
+  num2=parseInt(num2);
+  var r= num1/num2;
+  return r;
+}
+
+
+
 }
