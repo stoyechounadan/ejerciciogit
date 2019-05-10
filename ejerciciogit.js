e779a747c738627ea38c329d025dae1fa248058c
stoyechounadan <stoyechounadan@hotmail.com>
2019-05-10 12:53:55 +0200

    con la calculadora lista

--- ejerciciogit.js
+++ ejerciciogit.js
@@ -6,6 +6,22 @@ boton.addEventListener("click", eventoClickOperacion);
 
 function eventoClickOperacion(){
   num=num.value;
-resultado.innerHTML=num;
+  var result = calculaExpresion(num);
+  resultado.innerHTML=result;
+//for(i=0, i<num.length, i++){
+//  if(num[i]=="+"){
 
+
+
+}
+
+
+function calculaExpresion(exp){
+  var operador=exp.indexOf("+");
+  var num1=exp.substring(0,operador);
+  var num2=exp.slice(operador+1);
+  num1=parseInt(num1);
+  num2=parseInt(num2);
+  var r= num1+num2;
+  return r;
 }
